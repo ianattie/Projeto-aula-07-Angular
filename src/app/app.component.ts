dc0b52d9af68b2371897886b65f721edba4972ed
Ian Attie <attieian@gmail.com>
2026-04-18 09:44:23 -0300

    feat: calculadora estilo real com visor e teclado numérico

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,62 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FormsModule],
   templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  visor: string = '';
   num1: number = 0;
-  num2: number = 0;
-  resultado: number = 0;
+  oper: string = '';
+  novoNumero: boolean = false;
 
-  somar() {
-    this.resultado = this.num1 + this.num2;
-  }
-
-  subtrair() {
-    this.resultado = this.num1 - this.num2;
-  }
-
-  multiplicar() {
-    this.resultado = this.num1 * this.num2;
-  }
-
-  dividir() {
-    if (this.num2 !== 0) {
-      this.resultado = this.num1 / this.num2;
-    } else {
-      alert('Não pode dividir por zero');
+  adicionar(valor: string) {
+    if (this.novoNumero) {
+      this.visor = '';
+      this.novoNumero = false;
     }
+    this.visor += valor;
+  }
+
+  operacao(op: string) {
+    this.num1 = Number(this.visor);
+    this.oper = op;
+    this.novoNumero = true;
+  }
+
+  calcular() {
+    const num2 = Number(this.visor);
+    let resultado = 0;
+
+    switch (this.oper) {
+      case '+':
+        resultado = this.num1 + num2;
+        break;
+      case '-':
+        resultado = this.num1 - num2;
+        break;
+      case '*':
+        resultado = this.num1 * num2;
+        break;
+      case '/':
+        resultado = num2 !== 0 ? this.num1 / num2 : 0;
+        break;
+    }
+
+    this.visor = resultado.toString();
+    this.novoNumero = true;
+  }
+
+  limpar() {
+    this.visor = '';
+    this.num1 = 0;
+    this.oper = '';
+  }
+
+  apagar() {
+    this.visor = this.visor.slice(0, -1);
   }
 }
