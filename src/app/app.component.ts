73f0cd971efb0f1749d919bb7e699f3adc6d582d
Ian Attie <attieian@gmail.com>
2026-04-18 09:32:30 -0300

    feat: adiciona calculadora Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'calculadora-angular';
+  num1: number = 0;
+  num2: number = 0;
+  resultado: number = 0;
+
+  somar() {
+    this.resultado = this.num1 + this.num2;
+  }
+
+  subtrair() {
+    this.resultado = this.num1 - this.num2;
+  }
+
+  multiplicar() {
+    this.resultado = this.num1 * this.num2;
+  }
+
+  dividir() {
+    if (this.num2 !== 0) {
+      this.resultado = this.num1 / this.num2;
+    } else {
+      alert('Não pode dividir por zero');
+    }
+  }
 }
